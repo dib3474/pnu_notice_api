3dea078db3f8c8b8ae1c646ec1c49291b10d3552
dib3474 <95611065+dib3474@users.noreply.github.com>
2023-01-14 21:25:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
-
+from flask import Flask, request, json, jsonify
+from cse_notice import get_notice
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template('./index.html')
+@app.route("/5463216168", methods=['POST']) #get 으로 변경
+def get_data():
+    response = get_notice()
+    return response
