653fc4ddab5fbf7ee86a018f644a169111fa9229
dib3474 <95611065+dib3474@users.noreply.github.com>
2023-01-16 23:33:36 +0900

    Change methods

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, json, jsonify
 from cse_notice import get_notice
 app = Flask(__name__)
 
-@app.route("/notice", methods=['POST']) #get 으로 변경
+@app.route("/notice", methods=['GET']) #get 으로 변경
 def get_data():
     response = get_notice()
     return response
